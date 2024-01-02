8b05120b026f74705c89dec22994ea594b187c60
zilvinas <zilvis80@mail.ru>
2024-01-02 22:24:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('nero to blogo kas neiseitu i gera');
+console.log('turtingi metai' + 'laukia manes');
+console.log(' ');
+console.log('javascript', 'gera kalba');
+console.log('pirmas \n\n antras \n\n trecias');
+console.log('Žilvinas', 'Imbrasas \nnoriu but nepriklausomas');
+console.log(' ');
+console.log(
+  'Miega berželiai \nBaltam patale. \nEgle, eglele, \nIš kur tu žalia? \nKodėl nepagelto '
+);
+console.log('* * * * \n*     * \n*     *\n* * * *');
+console.log('* \n* * \n* * *');
+console.log('Zilvinas Imbrasas');
+console.log('Nenoriu but prirystas prie darbo vietos');
+alert('ivyko klaida');
+console.log(
+  '+----------+----------+ \n|  Vardas  |  Amzius  | \n+----------+----------+ \n|  Tomas   |  24      | \n\n|  Jonas   |  26      | \n\n|  Juste   |  25      | \n+----------+----------+'
+);
